142e270b30060659b7e25adf03cb4dfa1305a410
mbuthia wanjiru <sheezwagichuru.gmail.com>
2024-04-13 01:14:20 +0300

    project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Base URL for API requests
 const BaseUrl = "https://www.hebcal.com";
-
+// Selecting DOM elements
 const currentDate = document.querySelector(".current-date"),
     daysTag = document.querySelector(".days"),
     previousNextIcon = document.querySelectorAll(".icons span "),
@@ -8,10 +9,12 @@ const currentDate = document.querySelector(".current-date"),
 let date = new Date(),
     currentYear = date.getFullYear(),
     currentMonth = date.getMonth();
-
+// Array of month names in Hebrew sarting from January to December
 const months = ["ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני", "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"];
 
 const renderCalendar = () => {
+     // Get the first day of the current month, the last date of the current month, 
+    // the last day of the current month, and the last date of the previous month
     const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
     const lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
     const lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth).getDay();
@@ -35,8 +38,10 @@ const renderCalendar = () => {
     daysTag.innerHTML = liTag;
 }
 
+// Function to fetch data from the API
 const fetchData = async () => {
     try {
+        // Make a GET request to the API endpoint for the current month
         const response = await fetch(`${BaseUrl}/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=${currentMonth + 1}&ss=on&mf=on&c=on&geo=geoname&geonameid=3448439&M=on&s=on`);
         const data = await response.json();
         renderCalendar(data);
@@ -46,7 +51,7 @@ const fetchData = async () => {
 }
 
 fetchData();
-
+// Event listeners for previous and next month navigation
 previousNextIcon.forEach(icon => {
     icon.addEventListener("click", async () => {
         currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
@@ -62,7 +67,7 @@ previousNextIcon.forEach(icon => {
         await fetchData();
     })
 });
-
+// Event listener for the click event on the input button
 inputBtn.addEventListener("click", async () => {
     const inputMonthValue = document.getElementById("inputMonth").value;
     const inputDayValue = document.getElementById("inputDay").value;
@@ -73,8 +78,10 @@ inputBtn.addEventListener("click", async () => {
         currentMonth = month - 1;
         currentYear = new Date().getFullYear();
         date = new Date(currentYear, currentMonth, day);
+         // Fetch data for the specified date
         await fetchData();
     } else {
+        // If the input values are not valid, display an alert message
         alert("Please enter a valid month (1-12) and day (1-31).");
     }
 });
